a0f75d31edd84180a91e7fc4924179d087aa693e
Julien Simon <julien@julien.org>
2018-06-09 17:58:30 +0300

    Initial version

---
+++ ML/scikit/decisiontrees.py
@@ -0,0 +1,22 @@
+from sklearn.datasets import load_iris
+from sklearn import tree
+import graphviz 
+
+iris = load_iris()
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(iris.data, iris.target)
+
+dot_data = tree.export_graphviz(clf, out_file=None) 
+graph = graphviz.Source(dot_data) 
+graph.render("iris") 
+
+dot_data = tree.export_graphviz(clf, out_file=None, 
+                         feature_names=iris.feature_names,  
+                         class_names=iris.target_names,  
+                         filled=True, rounded=True,  
+                         special_characters=True)  
+graph = graphviz.Source(dot_data)  
+graph 
+
+print(clf.predict(iris.data[:1, :]))
+print(clf.predict_proba(iris.data[:1, :]))
