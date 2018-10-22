35c7aa7f56eb6bf841ffb83c582d23d0c17a2465
Julien Simon <julien@julien.org>
2018-10-22 23:42:48 +0200

    Clean up

--- ML/scikit/pca.py
+++ ML/scikit/pca.py
@@ -13,15 +13,13 @@ pca = PCA(n_components=2)
 X_r = pca.fit(X).transform(X)
 
 # Percentage of variance explained for each components
-print('explained variance ratio (first two components): %s'
-      % str(pca.explained_variance_ratio_))
+print('explained variance ratio: %s' % str(pca.explained_variance_ratio_))
 
 plt.figure()
-colors = ['navy', 'turquoise', 'darkorange']
-lw = 2
+colors = ['red', 'green', 'blue']
 
 for color, i, target_name in zip(colors, [0, 1, 2], target_names):
-    plt.scatter(X_r[y == i, 0], X_r[y == i, 1], color=color, alpha=.8, lw=lw, label=target_name)
+    plt.scatter(X_r[y == i, 0], X_r[y == i, 1], color=color, alpha=.8, lw=2, label=target_name)
 plt.legend(loc='best', shadow=False, scatterpoints=1)
 plt.title('PCA of IRIS dataset')
 
