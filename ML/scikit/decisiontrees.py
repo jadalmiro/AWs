799d7a9255d639340f19811e510721c397ebde02
Julien Simon <julien@julien.org>
2018-10-22 23:41:49 +0200

    Display metrics

--- ML/scikit/decisiontrees.py
+++ ML/scikit/decisiontrees.py
@@ -1,9 +1,9 @@
 from sklearn.datasets import load_iris
-from sklearn import tree
+from sklearn import tree, metrics
 import graphviz 
 
 iris = load_iris()
-clf = tree.DecisionTreeClassifier()
+clf = tree.DecisionTreeClassifier(max_depth=2)
 clf = clf.fit(iris.data, iris.target)
 
 dot_data = tree.export_graphviz(clf, out_file=None) 
@@ -18,5 +18,15 @@ dot_data = tree.export_graphviz(clf, out_file=None,
 graph = graphviz.Source(dot_data)  
 graph 
 
-print(clf.predict(iris.data[:1, :]))
-print(clf.predict_proba(iris.data[:1, :]))
+# Predict the full data set
+expected = iris.target
+predicted = clf.predict(iris.data)
+
+# Display metrics
+# Precision measures the impact of false positives: TP/(TP+FP)
+# Recall measures the impact of false negatives : TP/(TP+FN)
+# F1 is the weighted average of precision and recall: (2*Recall*Precision)/(Recall+Precision)
+print(metrics.classification_report(expected, predicted))
+
+# Display confusion matrix
+print(metrics.confusion_matrix(expected, predicted))
