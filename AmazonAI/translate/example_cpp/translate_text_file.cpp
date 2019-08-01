e97168e490d2b29a21fa5147dcb54a5cc5acc225
Simon <julsimon@a483e7212d48.ant.amazon.com>
2019-08-01 06:03:10 +0200

    Cleanup

--- AmazonAI/translate/example_cpp/translate_text_file.cpp
+++ AmazonAI/translate/example_cpp/translate_text_file.cpp
@@ -8,7 +8,7 @@
 #include <iostream>
 #include <string>
 
-# define MAX_LINE_LENGTH 5000
+const int MAX_LINE_LENGTH=5000;
 
 int main(int argc, char **argv) {
   if (argc != 4) {
@@ -18,17 +18,13 @@ int main(int argc, char **argv) {
     return -1;
   }
 
-  const Aws::String target_language = argv[1];
-  const std::string input_file = argv[2];
-  const std::string output_file = argv[3];
-
-  std::ifstream fin(input_file.c_str(), std::ios::in);
+  std::ifstream fin(argv[2], std::ios::in);
   if (!fin.good()) {
     std::cerr << "Input file is invalid." << std::endl;
     return -1;
   }
 
-  std::ofstream fout(output_file.c_str(), std::ios::out);
+  std::ofstream fout(argv[3], std::ios::out);
   if (!fout.good()) {
     std::cerr << "Output file is invalid." << std::endl;
     return -1;
@@ -37,9 +33,10 @@ int main(int argc, char **argv) {
   Aws::SDKOptions options;
   Aws::InitAPI(options);
   {
-    Aws::Translate::TranslateClient translate_client;
+    const Aws::Translate::TranslateClient translate_client;
+
     Aws::Translate::Model::TranslateTextRequest request;
-    request = request.WithSourceLanguageCode("auto").WithTargetLanguageCode(target_language);
+    request = request.WithSourceLanguageCode("auto").WithTargetLanguageCode(argv[1]);
 
     Aws::String line;
     while (getline(fin, line)) {
@@ -48,20 +45,19 @@ int main(int argc, char **argv) {
       }
 
       if (line.length() > MAX_LINE_LENGTH) {
-        std::cerr << "Line is over " << MAX_LINE_LENGTH << " characters long." << std::endl;
+        std::cerr << "Line is too long." << std::endl;
         break;
       }
 
       request.SetText(line);
-      auto outcome = translate_client.TranslateText(request);
 
+      auto outcome = translate_client.TranslateText(request);
       if (outcome.IsSuccess()) {
-        auto translation = outcome.GetResult().GetTranslatedText();
-        std::cout << translation << std::endl;
-        fout << translation << std::endl;
+        fout << outcome.GetResult().GetTranslatedText() << std::endl;
       } else {
-        std::cout << "TranslateText error: " << outcome.GetError().GetExceptionName()
-             << " - " << outcome.GetError().GetMessage() << std::endl;
+        auto error = outcome.GetError();
+        std::cout << "TranslateText error: " << error.GetExceptionName()
+             << " - " << error.GetMessage() << std::endl;
         break;
       }
     }
